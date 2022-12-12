1eee9c8c81c94174e8f438bb841f3a215f07e510
Mir Moheen <mirmoheen9@gmail.com>
2022-12-13 01:36:04 +0500

    user reg

--- EcommereceStore/Controllers/HomeController.cs
+++ EcommereceStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EcommereceStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,36 @@ namespace EcommereceStore.Controllers
 {
     public class HomeController : Controller
     {
+        DbEntities DbEntities = new DbEntities();
         public ActionResult Index()
         {
             return View();
         }
-
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Alluser alluser)
+        {
+            DbEntities.Allusers.Add(alluser);
+            DbEntities.SaveChanges();
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Alluser alluser)
+        {
+            var data = DbEntities.Allusers.Where(x => x.useremail == alluser.useremail && x.UserPass == alluser.UserPass).FirstOrDefault();
+            if (data != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
